7c5911fcc43e3827a412dc5ca9c6c3a1d0b69d74
Jota <jjimenez@zonar.cl>
2022-01-24 14:30:46 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const version = "0.63";
 const log = require("./lib/Logs")
 const importer = require("./lib/Importer")
 
 importer.init();
-log.info("GEOOS GEOServer initialized");
-console.log("GEOOS GEOServer [0.63] is Running")
+log.info("GEOOS GEOServer [" + version + " ]initialized");
+console.log("GEOOS GEOServer [" + version + "] is Running")
