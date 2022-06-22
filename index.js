b924951981b0e1311009b2db97c20df7169dbc7e
Jota <jjimenez@zonar.cl>
2022-06-22 17:08:11 -0400

    deleter

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const version = "0.65";
 const log = require("./lib/Logs")
 const importer = require("./lib/Importer")
+const deleter = require("./lib/Deleter")
 
 importer.init();
+deleter.init();
 log.info("GEOOS GEOServer [" + version + " ]initialized");
 console.log("GEOOS GEOServer [" + version + "] is Running")
